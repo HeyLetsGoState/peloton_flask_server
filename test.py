732b473c997704fec4fcb5b11188f21eec2a444b
Bryan Kardisco <bryan.kardisco@gmail.com>
2020-10-31 20:58:06 -0400

    Added music playlist

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from connection.peloton_connection import PelotonConnection
 
-data = '{"username_or_email":"your_email_goes_here","password":"your_password_goes_here"}'
+data = '{"username_or_email":"NONE","password":"NONE"}'
 
 # Create the Peloton Connection
 conn = PelotonConnection()
@@ -14,5 +14,5 @@ user_id = auth_response.get("user_id")
 cookies = dict(peloton_session_id=session_id)
 
 # Run this daily or set-up a cron to do it for you
-conn.get_most_recent_ride_details(user_id, cookies)
-conn.get_most_recent_ride_info(user_id, cookies)
+conn.get_most_recent_ride_details(user_id, cookies, False)
+conn.get_most_recent_ride_info(user_id, cookies, False)
