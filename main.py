919984043fba6e9111a29b95d151fd07326a3469
EC2 Default User <ec2-user@ip-172-31-63-43.ec2.internal>
2020-11-02 21:29:02 +0000

    fix

--- main.py
+++ main.py
@@ -313,10 +313,10 @@ def load_user(userid):
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'http://pelodashboard.com')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    #response.headers.add('Access-Control-Allow-Origin', 'http://pelodashboard.com')
+    #response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    #response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    #response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
 
 
